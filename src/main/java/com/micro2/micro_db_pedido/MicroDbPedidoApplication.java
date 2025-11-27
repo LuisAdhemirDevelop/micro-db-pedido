cce3ce24407f8e0c59cf24b6d3ced1741811556e
Luis Añazgo <luis.adhemir.develop@gmail.com>
2025-11-27 17:44:47 -0500

    subiendo repositorio

---
+++ src/main/java/com/micro2/micro_db_pedido/MicroDbPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.micro2.micro_db_pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroDbPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroDbPedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/micro2/micro_db_pedido/MicroDbPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro2.micro_db_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroDbPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
